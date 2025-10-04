8fa149c21405db625e04a15011e29984b273b970
kuro <devandresvargas@gmail.com>
2025-10-03 23:39:22 -0500

    fix ftplugin dart/python

---
+++ ftplugin/python.lua
@@ -0,0 +1,13 @@
+-- Indentación para Python (PEP 8 recomienda 4 espacios)
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
+
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+
+-- Color de guía de columnas en 79 caracteres (PEP 8)
+vim.wo.colorcolumn = "79"
+
+
