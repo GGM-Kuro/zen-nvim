253e637b94a107071a553afb6a8afe6ff7c804ad
kuro <devandresvargas@gmail.com>
2025-10-06 20:51:53 -0500

    fix dap

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -143,7 +143,7 @@ return {
 				-- The nvim-dap plugin populates this variable with the editor's current working directory
 				cwd = "${workspaceFolder}",
 				-- This gets forwarded to the Flutter CLI tool, substitute `linux` for whatever device you wish to launch
-				toolArgs = { "-d", "Android" },
+				-- toolArgs = { "-d", "Android" },
 			},
 		}
 	end,
