79e554dae44f04d1031c2163414cea877bc4c62a
kuro <devandresvargas@gmail.com>
2025-10-23 22:49:44 -0500

    fix snacks picker

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -11,7 +11,7 @@ return {
 		indent = { enabled = true },
 		input = { enabled = true },
 		image = { enabled = true },
-		picker = { enabled = true, ui_select = true },
+		picker = { enabled = true, ui_select = false },
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
