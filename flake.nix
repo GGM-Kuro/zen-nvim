7c85152a19e287020192dd282a69fc08723456e7
kuro <devandresvargas@gmail.com>
2025-10-23 21:45:40 -0500

    fix dap menu

--- flake.nix
+++ flake.nix
@@ -149,6 +149,7 @@
               pkgs.neovimPlugins.debugmaster
               pkgs.neovimPlugins.quicktype
               pkgs.neovimPlugins.winpick
+              hydra-nvim
               nvzone-volt
               nvzone-typr
               fidget-nvim
