0dd2a1388744f65b38963ba58704e1022f8493f7
kuro <devandresvargas@gmail.com>
2026-01-31 16:00:20 -0500

    .

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,6 @@ return {
 	{ "MagicDuck/grug-far.nvim", enabled = false },
 	{ "folke/flash.nvim", enabled = true },
 	-- { "folke/trouble.nvim", enabled = false },
-	-- { "akinsho/bufferline.nvim", enabled = false },
+	{ "akinsho/bufferline.nvim", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
 }
