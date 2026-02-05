04e5313a6d779964378e8bbca9849dd4d659e87d
kuro <devandresvargas@gmail.com>
2026-02-04 20:24:27 -0500

    fix reload LSP keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,83 @@
-vim.keymap.set("n", "<Tab>", vim.cmd.bnext)
-vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)
-vim.keymap.set("n", "<leader>o", "o<Esc>k")
-vim.keymap.set("n", "<leader>x", vim.cmd.dbelete)
-vim.keymap.set("n", "<leader>y", '"+yy', { noremap = true })
-vim.keymap.set("n", "<leader>dy", "3<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>fi", "/", { noremap = true })
-vim.keymap.set("n", "<leader>a", "ggVG", { noremap = true })
+local harpoon = require("harpoon")
+local key = require("which-key")
+
+harpoon:setup({})
+
+key.add({
+	-- =========================
+	-- BUFFER NAVIGATION
+	-- =========================
+	{
+		"<S-Tab>",
+		vim.cmd.bprevious,
+		desc = "Buffer previous",
+		icon = "⬅️",
+	},
+	{
+		"<Tab>",
+		vim.cmd.next,
+		desc = "Buffer next",
+		icon = "➡️",
+	},
+
+	-- =========================
+	-- EDITING
+	-- =========================
+	{
+		"<leader>o",
+		"o<Esc>k",
+		desc = "Insert line below (stay normal)",
+		icon = "⤵️",
+	},
+	{
+		"<leader>x",
+		vim.cmd.bdelete,
+		desc = "Delete buffer",
+		icon = "",
+	},
+	{
+		"<leader>a",
+		"ggVG",
+		desc = "Select all",
+		icon = "󰒆",
+	},
+
+	-- =========================
+	-- CLIPBOARD
+	-- =========================
+	{
+		"<leader>y",
+		'"+yy',
+		desc = "Yank line to system clipboard",
+		icon = "",
+	},
+
+	-- =========================
+	-- DIAGNOSTICS
+	-- =========================
+	{
+		"<leader>dy",
+		function()
+			vim.diagnostic.open_float()
+		end,
+		desc = "Show diagnostics (float)",
+	},
+    {
+		"<s-r>",
+		function()
+			vim.cmd.LspRestart()
+		end,
+		desc = "LSP restart",
+		icon = "🔄",
+	},
+
+	-- =========================
+	-- SEARCH
+	-- =========================
+	{
+		"<leader>fi",
+		"/",
+		desc = "Search forward",
+		icon = "",
+	},
+})
